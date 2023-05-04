ca00cbafbc71d8c4e1726a94e5e3305422662e4f
0rn <orangelord721@gmail.com>
2023-05-04 14:51:28 +1000

    start again with nvim 0.9

---
+++ lua/plugins.lua
@@ -0,0 +1,5 @@
+return {
+	"folke/which-key.nvim",
+	"williamboman/mason.nvim",
+	build = ":MasonUpdate" -- :MasonUpdate updates registry contents
+}
---
+++ lua/plugins/nim.lua
@@ -0,0 +1,5 @@
+local M = {
+	"baabelfish/nvim-nim"
+}
+
+return M
